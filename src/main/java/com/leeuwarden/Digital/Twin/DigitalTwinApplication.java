b4d201d257a14a3a1a769003baad55639c662fe1
Firas <firaskayyal03@gmail.com>
2025-11-25 17:11:04 +0100

    Initial commit

---
+++ src/main/java/com/leeuwarden/Digital/Twin/DigitalTwinApplication.java
@@ -0,0 +1,13 @@
+package com.leeuwarden.Digital.Twin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalTwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalTwinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leeuwarden/Digital/Twin/DigitalTwinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeuwarden.Digital.Twin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalTwinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
